a5160be73c2eb681f203207f8e18957a8a368516
jpatterson <j_patterson24@hotmail.com>
2017-10-14 00:28:10 -0400

    Updated Hangman code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 
 //Variable List==============================================================================================================================================
-var wordOptions = ["family matters", "fullhouse", "dinosaurs", "blossom", "martin", "the fresh prince of belair"];
+var wordOptions = ["family matters", "fullhouse", "dinosaurs", "blossom", "martin", "the fresh prince of belair", "xfiles", "seinfeld", "boy meets world", "friends", "home improvement", "saved by the bell", "the simpsons", "the cosby show", "cheers"];
 var selectedWord =  "";
 var lettersinWord = [];
 var numBlanks = 0;
